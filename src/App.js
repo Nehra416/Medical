cfcfacd97f1febc038d8438e323f924db36863f0
veena1vr <veenaluhaniwal.com>
2024-07-19 11:42:52 +0530

    "changes"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/Pages/Home";
 import Service from "./Components/Pages/Service";
-import Single_service from "./Components/Pages/Single_service";
- import About from "./Components/Pages/About";
+  import About from "./Components/Pages/About";
 import Ads from "./Components/Ads";
  import Doctors from "./Components/Home/Doctors";
  import Maindata from "./Component/Maindata";
